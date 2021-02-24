a6878605f2f32ad0b5de6c993c7e59d07a560597
jairodias <jairopereira_dias@hotmail.com>
2021-02-24 06:32:29 -0300

    [master] - Criando principios de autenticaçao dentro do site

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'indent': 'off',
-        'eol-last': ["error", "never"]
+        'eol-last': ["error", "never"],
+        "function-paren-newline": ["error", "never"]
     },
     overrides: [{
         files: [
